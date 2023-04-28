32c3dcc9ca7ea44ee52c7c5ef4ab45c7b9213434
Misha Tuesday <mishatuesday@gmail.com>
2023-04-28 09:28:28 -0400

    adding local work to remote repo

---
+++ src/Component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Component() {
+    return (
+        <div>
+            This is a placeholder. If you have questions or comments you may email them to <a href="mailto:mishatuesday@gmail.com">the developer</a>
+        </div>
+    )
+}
+
+export default Component
